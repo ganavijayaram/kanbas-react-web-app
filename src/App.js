dbfc447f0b3c79965dfad6e1d54b9094c7bf8a84
ganavijayaram <jayaram.g@northeastern.edu>
2023-10-18 19:36:48 -0400

    Lab 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+/*import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -21,5 +21,24 @@ function App() {
     </div>
   );
 }
+*/
 
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import Kanbas from "./Kanbas";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
 export default App;
---
+++ src/Labs/a3/JsonStringify.js
@@ -0,0 +1,7 @@
+const squares = [1, 4, 16, 25, 36];
+
+
+
+
+<h3>JSON Stringify</h3>
+squares = { JSON.stringify(squares) } <br />
